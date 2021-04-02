8c7aac2ed7c098fa0a6e9b0cb1f17b856f42279f
Turlough Mullan <duke1199sp@gmail.com>
2021-04-02 16:23:51 +0200

    LAwwwwd there is so much to do

--- error/error.c
+++ error/error.c
@@ -6,7 +6,7 @@
 /*   By: zenotan <zenotan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/16 13:14:11 by zenotan       #+#    #+#                 */
-/*   Updated: 2021/04/02 12:43:44 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/04/02 12:54:49 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,10 @@ void	cmd_notfound(t_cmd *cmd, int flag, t_shell **ghost)
 	}
 	else if (!cmd->redirection)
 	{
-		(*ghost)->ret_stat = 69;
+		(*ghost)->ret_stat = NOT_CMD;
 
 		// (*ghost)->ret_stat = NOT_CMD;
-		ft_putnbr_fd((*ghost)->ret_stat, 1);
+		// ft_putnbr_fd((*ghost)->ret_stat, 1);
 		ft_putstr_fd(cmd->type, 1);
 		ft_putstr_fd(": command not found\n", 1);
 	}
