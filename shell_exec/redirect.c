124cee6184bde10bb1e07a5e0a548ac738974661
Turlough Mullan <duke1199sp@gmail.com>
2021-05-20 16:06:32 +0200

    motherfucker been normed now too

--- shell_exec/redirect.c
+++ shell_exec/redirect.c
@@ -6,103 +6,43 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/11 12:45:04 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/05/20 15:51:51 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/05/20 16:06:08 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ghostshell.h"
 
-int	open_multi(void *file_struct)
+int	multi_redir(t_cmd *cmd, t_shell **ghost, t_redir *file_struct)
 {
-	int		fd;
-	t_redir	*filename;
+	int	fd;
 
-	filename = (t_redir *)file_struct;
-	if (filename->type == 0)
-		fd = open(filename->file, O_CREAT | O_TRUNC | O_RDWR, 0666);
+	if (file_struct->type == INPUT)
+	{
+		(*ghost)->error = NO_FILE;
+		fd = open(file_struct->file, O_APPEND | O_RDWR, 0666);
+		if (fd == -1)
+		{
+			cmd_notfound(cmd, NO_FILE, ghost, 0);
+			return (-1);
+		}
+		(*ghost)->in = dup(STDIN_FILENO);
+		dup2(fd, STDIN_FILENO);
+	}
 	else
-		fd = open(filename->file, O_CREAT | O_APPEND | O_RDWR, 0666);
-	// else
-	// {
-	// 	fd = open(filename->file, O_APPEND | O_RDWR, 0666);
-	// 	if (fd == -1)
-	// 	{
-	// 		cmd_notfound(cmd, NO_FILE, ghost, 0);
-	// 		return (-1);
-	// 	}
-	// }
-	return (fd);
+	{
+		if (file_struct->type == OUTPUT)
+			fd = open(file_struct->file, O_CREAT | O_TRUNC | O_RDWR, 0666);
+		else
+			fd = open(file_struct->file, O_CREAT | O_APPEND | O_RDWR, 0666);
+	}
+	close(fd);
+	return (0);
 }
 
-// int	multi_redir(t_cmd *cmd, t_shell **ghost, int num, t_redir *file_struct)
-// {
-// 	int	fd;
-// 	int	original;
-
-// 	if (file_struct->type == INPUT)
-// 	{
-// 		(*ghost)->error = NO_FILE;
-// 		fd = open(file_struct->file, O_APPEND | O_RDWR, 0666);
-// 		if (fd == -1)
-// 		{
-// 			cmd_notfound(cmd, NO_FILE, ghost, 0);
-// 			return (-1);
-// 		}
-// 		if (num == 0)
-// 		{
-// 			original = dup(STDIN_FILENO);
-// 			dup2(fd, STDIN_FILENO);
-// 		}
-// 		else
-// 		{
-// 			dup2()
-// 		}
-// 	}
-// 	else
-// 	{
-// 		if (file_struct->type == OUTPUT)
-// 			fd = open(file_struct->file, O_CREAT | O_TRUNC | O_RDWR, 0666);
-// 		else
-// 			fd = open(file_struct->file, O_CREAT | O_APPEND | O_RDWR, 0666);
-// 		original = dup(STDOUT_FILENO);
-// 		dup2(fd, STDOUT_FILENO);
-// 	}
-// 	close(fd);
-// 	// return(original);
-// }
-
-int	redirect(t_cmd *cmd, t_shell **ghost)
+int	final_redir(t_cmd *cmd, t_shell **ghost, t_redir *file_struct)
 {
-	t_redir	*file_struct;
-	int		fd;
+	int	fd;
 
-	file_struct = (t_redir *)cmd->redirection->content;
-	while (cmd->redirection->next)
-	{
-		// multi_redir(cmd, ghost, original, file_struct);
-		if (file_struct->type == INPUT)
-		{
-			(*ghost)->error = NO_FILE;
-			fd = open(file_struct->file, O_APPEND | O_RDWR, 0666);
-			if (fd == -1)
-			{
-				cmd_notfound(cmd, NO_FILE, ghost, 0);
-				return (-1);
-			}
-			(*ghost)->in = dup(STDIN_FILENO);
-			dup2(fd, STDIN_FILENO);
-		}
-		else
-		{
-			if (file_struct->type == OUTPUT)
-				fd = open(file_struct->file, O_CREAT | O_TRUNC | O_RDWR, 0666);
-			else
-				fd = open(file_struct->file, O_CREAT | O_APPEND | O_RDWR, 0666);
-		}
-		close(fd);
-		cmd->redirection = cmd->redirection->next;
-		file_struct = cmd->redirection->content;
-	}
 	if (file_struct->type == INPUT)
 	{
 		(*ghost)->error = NO_FILE;
@@ -125,5 +65,22 @@ int	redirect(t_cmd *cmd, t_shell **ghost)
 		dup2(fd, STDOUT_FILENO);
 	}
 	close(fd);
+	return (0);
+}
+
+int	redirect(t_cmd *cmd, t_shell **ghost)
+{
+	t_redir	*file_struct;
+
+	file_struct = (t_redir *)cmd->redirection->content;
+	while (cmd->redirection->next)
+	{
+		if (multi_redir(cmd, ghost, file_struct) == -1)
+			return (-1);
+		cmd->redirection = cmd->redirection->next;
+		file_struct = cmd->redirection->content;
+	}
+	if (final_redir(cmd, ghost, file_struct) == -1)
+		return (-1);
 	return (0);
 }
