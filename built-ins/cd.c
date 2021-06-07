f496bef95253ecd70d500959b3629b30a4d309bb
Turlough Mullan <duke1199sp@gmail.com>
2021-06-07 11:04:19 +0200

    cd without args

--- built-ins/cd.c
+++ built-ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/22 12:40:53 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/05/22 12:44:57 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/06/07 11:03:58 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,33 @@ void	cd_home(t_shell **ghost, t_cmd *cmd)
 	}
 }
 
+void	cd_root(t_shell **ghost)
+{
+	int	i;
+
+	i = 0;
+	while ((*ghost)->env[i])
+	{
+		if (ft_strnstr((*ghost)->env[i], "HOME", ft_strlen("HOME"))
+			!= 0)
+		{
+			if (chdir(&(*ghost)->env[i][5]) != 0)
+				strerror(errno);
+		}
+		i++;
+	}
+}
+
 int	run_cd(t_cmd *cmd, t_shell **ghost)
 {
 	int	i;
 
 	i = 0;
 	if (cmd->args == NULL)
+	{
+		cd_root(ghost);
 		return (1);
+	}
 	if (ft_strcmp(cmd->args->content, "-") == 0)
 	{
 		ft_putstr_fd("Ghostshell does not support this var expansion\n", 1);
